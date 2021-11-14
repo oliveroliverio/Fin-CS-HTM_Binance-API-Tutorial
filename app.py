82c02ebb3543c83ac030edded31c6e7adeb90c70
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-14 15:48:21 -0800

    2021114-part06: commented app.py code (important!)

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
+# create new flask application, which is a flask object
 app = Flask(__name__)
 
+# define routes
 @app.route('/')
+
+# define functions for when the route is called into the browser
 def hello_world():
     return 'Hello, World!'
